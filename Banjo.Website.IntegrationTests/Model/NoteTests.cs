96152adbb0f4160167cd1d89386398722c5dcade
SiCannon <simon.cannon@gmail.com>
2015-10-26 22:45:23 +0000

    Stuff

--- Banjo.Website.IntegrationTests/Model/NoteTests.cs
+++ Banjo.Website.IntegrationTests/Model/NoteTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Banjo.Website.Database;
+using Banjo.Website.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Banjo.Website.IntegrationTests.Model
@@ -10,8 +11,12 @@ namespace Banjo.Website.IntegrationTests.Model
         [TestMethod]
         public void Can_Save_Note()
         {
+            var note = new Note { Text = "Test Note " + DateTime.Now.ToString() };
             var db = new BanjoContext();
-
+            db.Notes.Add(note);
+            db.SaveChanges();
+            Assert.IsNotNull(note.NoteId);
+            Assert.AreNotEqual(0, note.NoteId);
         }
     }
 }
