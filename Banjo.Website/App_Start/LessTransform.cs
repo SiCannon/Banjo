daf2e58a418fbb3bcb045d59ed36fd929db3e31c
SiCannon <simon.cannon@gmail.com>
2015-11-09 09:10:28 +0000

    Added dotless

---
+++ Banjo.Website/App_Start/LessTransform.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Optimization;
+using dotless.Core;
+
+namespace Banjo.Website
+{
+    public class LessTransform : IBundleTransform
+    {
+        string path;
+
+        public LessTransform(string path)
+        {
+            this.path = HttpContext.Current.Server.MapPath(path);
+        }
+
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            Directory.SetCurrentDirectory(path);
+
+            response.Content = Less.Parse(response.Content);
+            response.ContentType = "text/css";
+        }
+    }
+}
