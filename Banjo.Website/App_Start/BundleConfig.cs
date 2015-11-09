a81fb1caf8d9604f785f9ea0bae32332b250a837
SiCannon <simon.cannon@gmail.com>
2015-11-09 09:10:45 +0000

    Added dotless

--- Banjo.Website/App_Start/BundleConfig.cs
+++ Banjo.Website/App_Start/BundleConfig.cs
@@ -14,6 +14,11 @@ namespace Banjo.Website
             bundles.Add(new ScriptBundle("~/bundles/angular-banjo")
                 .Include("~/JsBanjo/Modules/BanjoApp.js")
                 .IncludeDirectory("~/JsBanjo", "*.js", true));
+
+            var less = new Bundle("~/bundles/less").Include("~/Styles/node.less");
+            less.Transforms.Add(new LessTransform("~/Styles"));
+            less.Transforms.Add(new CssMinify());
+            bundles.Add(less);
         }
     }
 }
